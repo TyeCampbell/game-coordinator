b0f75cedec4aaa98bdf49625fd108cd16a85e2b7
Tye Campbell <tcampbell@hy-vee.com>
2020-10-04 18:15:01 -0500

    initial files PR

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TimedMission from './components/mission_timed';
+import ControlPointMission from "./components/mission_control_point";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Choose Your Mission
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TimedMission/>
       </header>
     </div>
   );
