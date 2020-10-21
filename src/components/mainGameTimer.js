4e432927a390dd47df3404fb31c92b3517185c95
Tye Campbell <tcampbell@hy-vee.com>
2020-10-20 20:39:02 -0500

    mainGameTimer is working

--- src/components/mainGameTimer.js
+++ src/components/mainGameTimer.js
@@ -1,5 +1,11 @@
 import React, {useEffect, useRef, useState} from "react";
 
+const incomingPropTime = {
+    hours: 0,
+    minutes: 0,
+    seconds: 10
+}
+
 const useInterval = (callback, delay) => {
     const intervalId = useRef(null);
     const savedCallback = useRef(callback);
@@ -23,23 +29,60 @@ const useInterval = (callback, delay) => {
 
 const MainGameTimer = () => {
     const [status, setStatus] = useState("idle");
-    const [timeElapsed, setTimeElapsed] = useState(0);
+    const [timeRemaining, setTimeRemaining] = useState(incomingPropTime);
 
     useInterval(
         () => {
-            setTimeElapsed(timeElapsed => timeElapsed - 1);
+            setTimeRemaining(timeRemaining => {
+                const {hours, minutes, seconds} = timeRemaining;
+
+                if (hours > 0 && minutes === 0 && seconds === 0) {
+                    return {
+                        hours: hours - 1,
+                        minutes: 59,
+                        seconds: 59
+                    }
+                }
+
+                if (minutes > 0 && seconds === 0) {
+                    return {
+                        hours: hours,
+                        minutes: minutes -1,
+                        seconds: 59
+                    }
+                }
+
+                if (seconds > 0) {
+                    return {
+                        hours: hours,
+                        minutes: minutes,
+                        seconds: seconds -1
+                    }
+                }
+
+                return {
+                    hours: 0,
+                    minutes: 0,
+                    seconds: 0
+                }
+            });
         },
         status === "running" ? 1000 : null
     );
 
     const toggle = () => {
-        setTimeElapsed(100);
+        setTimeRemaining(incomingPropTime);
         setStatus(status => (status === "running" ? "idle" : "running"));
     };
 
+    const formatedTimer = (time) => {
+        const {hours, minutes, seconds} = time;
+        return `${("0" + hours).slice(-2)}:${("0" + minutes).slice(-2)}:${("0" + seconds).slice(-2)}`
+    }
+
     return (
         <div>
-            Time Remaining: {timeElapsed}
+            <h1>{formatedTimer(timeRemaining)}</h1>
             <br />
             <button onClick={toggle}>
                 {status === "running" ? "Stop" : "Start"}
